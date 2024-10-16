4526e3e7b01aede5ff1875bbe6a21860fce39263
Macey BO <91503771+virtualbomy@users.noreply.github.com>
2024-10-16 16:19:37 -0600

    Update bisiesto.asm

--- bisiesto.asm
+++ bisiesto.asm
@@ -2,13 +2,13 @@ section .text
 global calcularBisiesto
 
 calcularBisiesto:
-    mov rax, rdi      ; Mover el año (número) a RAX
+    mov rax, rdi      ;Mover el año (número) a RAX
 
 .bisiesto:
-    mov rax, 0        ; Retornar 0 si es bisiesto
+    mov rax, 0        ;Retornar 0 si es bisiesto
     ret
 
 .no_bisiesto:
-    mov rax, 1        ; Retornar 1 si no es bisiesto
+    mov rax, 1        ;Retornar 1 si no es bisiesto
     ret
 
